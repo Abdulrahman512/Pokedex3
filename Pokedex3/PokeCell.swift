7ded470b337067be40a22d0049a81e97065fc315
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-03-24 21:28:35 -0700

    Add PokeCell view class and hook it to storyboard

---
+++ Pokedex3/PokeCell.swift
@@ -0,0 +1,26 @@
+//
+//  PokeCell.swift
+//  Pokedex3
+//
+//  Created by Khalid Alrashed on 3/24/17.
+//  Copyright © 2017 Khalid Alrashed. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
