09cabaf0fd7e479d2da983f7c9909b7593b6630c
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-03-24 21:08:41 -0700

    Add Pokemon model class

---
+++ Pokedex3/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  Pokedex3
+//
+//  Created by Khalid Alrashed on 3/24/17.
+//  Copyright © 2017 Khalid Alrashed. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    
+}
